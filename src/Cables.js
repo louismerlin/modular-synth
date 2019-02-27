1375a97c7dce1280800288b620c8c949937e7685
Louis Merlin <louis.merlin@epfl.ch>
2019-02-27 13:14:10 +0100

    Fix cables offset

--- src/Cables.js
+++ src/Cables.js
@@ -70,11 +70,11 @@ class CablesOverlay extends Component {
       <svg class={overlayClass}>
         {cables.map(({ nodes, id }) => {
           const b1 = nodes[0].component.base
-          const x1 = b1.offsetLeft + b1.offsetWidth / 2 - 1
-          const y1 = b1.offsetTop + b1.offsetHeight / 2 - 1
+          const x1 = b1.offsetLeft + b1.offsetParent.offsetLeft + b1.offsetWidth / 2 - 1
+          const y1 = b1.offsetTop + b1.offsetParent.offsetTop + b1.offsetHeight / 2 - 1
           const b2 = nodes[1].component.base
-          const x2 = b2.offsetLeft + b2.offsetWidth / 2 - 1
-          const y2 = b2.offsetTop + b2.offsetHeight / 2 - 1
+          const x2 = b2.offsetLeft + b2.offsetParent.offsetLeft + b2.offsetWidth / 2 - 1
+          const y2 = b2.offsetTop + b2.offsetParent.offsetTop + b2.offsetHeight / 2 - 1
           return (<line class='cable' x1={x1} y1={y1} x2={x2} y2={y2} onClick={() => this.removeCable(id)} />)
         })}
       </svg> 
